9929e9003edaa7fbcc974f58c746e9e85b3b82a3
RomanGit777 <rsavenko91@gmail.com>
2025-11-20 14:13:22 +0100

    added MoviesPage.tsx

--- src/router/routes.tsx
+++ src/router/routes.tsx
@@ -1,6 +1,9 @@
 import {createBrowserRouter} from "react-router-dom";
 import MainLayout from "../layout/MainLayout.tsx";
+import {MoviesPage} from "../pages/MoviesPage.tsx";
 
 export const routes = createBrowserRouter([
-    {path:'/', element: <MainLayout/>}
+    {path:'/', element: <MainLayout/>, children:[
+            {path: 'movies', element: <MoviesPage/>}
+        ]}
 ])
