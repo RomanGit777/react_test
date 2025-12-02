9014956de86b3e84e04f00914c8f8e51c513cd49
RomanGit777 <rsavenko91@gmail.com>
2025-12-02 15:01:23 +0100

    done with adding cards+css

--- src/components/MovieCard/MovieCard.tsx
+++ src/components/MovieCard/MovieCard.tsx
@@ -1,28 +1,32 @@
 import './MovieCard.css'
-import {useEffect, useState} from "react";
 import type {IMovie} from "../../models/IMovie.ts";
-import {getMovies} from "../../api/getMovies.ts";
 
 
-export const MovieCard = () => {
-const [movie, setMovie] = useState<IMovie[]>([])
-useEffect(() => {
-    getMovies().then(movies => setMovie(movies))
-},[])
+interface MovieCardProps {
+    movie: IMovie
+}
+
+export const MovieCard = ({movie}: MovieCardProps) => {
+    const posterUrl = movie.poster_path
+        ? `https://image.tmdb.org/t/p/w500${movie.poster_path}`
+        : "/assets/placeholder.jpg";
+
+    const shortOverview = movie.overview ? movie.overview.slice(0, 80) + "…" : "";
+
+
     return (
-        <div className={'movie-card-wrapper'}>
-            <div className={'carousel'}>
-                <div className={'grid'}>
-                    <div  className={'card'}> {movie.map(movie =>
-                            <div key={movie.id}>
-                                <img
-                                src={`https://image.tmdb.org/t/p/w300${movie.poster_path}`}
-                                alt={movie.title}
-                                />
-                            </div>)}
+            <div className="card" role="button" tabIndex={0} aria-label={movie.title}>
+                    <img className="card-img" src={posterUrl} alt={movie.title} />
+
+                <div className="overlay">
+                        <div className="overlay-top">
+                            <span className="badge">Genre</span>
+                            <span className="rating">★ {Math.round(movie.vote_average * 10) / 10}</span>
+                        </div>
+
+                        <h4 className="overlay-title">{movie.title}</h4>
+                        <p className="overlay-desc">{shortOverview}</p>
                     </div>
-                </div>
-            </div>
         </div>
     );
 };
