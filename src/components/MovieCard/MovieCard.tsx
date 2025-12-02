e541565885936cb74d2e20340f403444550657c7
RomanGit777 <rsavenko91@gmail.com>
2025-12-02 14:00:29 +0100

    start new carousel

--- src/components/MovieCard/MovieCard.tsx
+++ src/components/MovieCard/MovieCard.tsx
@@ -1,14 +1,26 @@
 import './MovieCard.css'
+import {useEffect, useState} from "react";
+import type {IMovie} from "../../models/IMovie.ts";
+import {getMovies} from "../../api/movieSeries.ts";
+
 
 export const MovieCard = () => {
+const [movie, setMovie] = useState<IMovie[]>([])
+useEffect(() => {
+    getMovies().then(movies => setMovie(movies))
+},[])
     return (
         <div className={'movie-card-wrapper'}>
             <div className={'carousel'}>
                 <div className={'grid'}>
-                    <div className={'card'}></div>
-                    <div className={'card'}></div>
-                    <div className={'card'}></div>
-                    <div className={'card'}></div>
+                    <div  className={'card'}> {movie.map(movie =>
+                            <div key={movie.id}>
+                                <img
+                                src={`https://image.tmdb.org/t/p/w300${movie.poster_path}`}
+                                alt={movie.title}
+                                />
+                            </div>)}
+                    </div>
                 </div>
             </div>
         </div>
