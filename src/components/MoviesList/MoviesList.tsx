2541414f71abb6172943339d7cf442b447830aa4
RomanGit777 <rsavenko91@gmail.com>
2025-12-02 15:01:23 +0100

    done with adding cards+css

--- src/components/MoviesList/MoviesList.tsx
+++ src/components/MoviesList/MoviesList.tsx
@@ -1,9 +1,25 @@
 import './MoviesList.css'
 import {MovieCard} from "../MovieCard/MovieCard.tsx";
+import {useEffect, useState} from "react";
+import type {IMovie} from "../../models/IMovie.ts";
+import {getMovies} from "../../api/getMovies.ts";
 export const MoviesList = () => {
+    const [movies, setMovies] = useState<IMovie[]>([])
+    const [loading, setLoading] = useState(true)
+    useEffect(() => {
+        getMovies().then((data) => setMovies(data))
+            .catch((err) =>
+                console.log('Failed to load movies', err))
+            .finally(() => setLoading(false))
+    },[])
+
+    if(loading) return <div>Loading...</div>
+    if(!movies.length) return <div>No movies found</div>
+
     return (
-        <div className={'movies-list-wrapper'}>
-            <MovieCard/>
+        <div className={'grid'}>
+            {movies.map((m) =>
+                <MovieCard key={m.id} movie={m} />)}
         </div>
     );
 };
