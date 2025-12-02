c3c0f2d6c549af3f7ad46ee3b2dde4472bd49486
RomanGit777 <rsavenko91@gmail.com>
2025-12-02 15:32:14 +0100

    added pagination

--- src/components/MoviesList/MoviesList.tsx
+++ src/components/MoviesList/MoviesList.tsx
@@ -1,25 +1,17 @@
 import './MoviesList.css'
-import {MovieCard} from "../MovieCard/MovieCard.tsx";
-import {useEffect, useState} from "react";
-import type {IMovie} from "../../models/IMovie.ts";
-import {getMovies} from "../../api/getMovies.ts";
-export const MoviesList = () => {
-    const [movies, setMovies] = useState<IMovie[]>([])
-    const [loading, setLoading] = useState(true)
-    useEffect(() => {
-        getMovies().then((data) => setMovies(data))
-            .catch((err) =>
-                console.log('Failed to load movies', err))
-            .finally(() => setLoading(false))
-    },[])
+import { MovieCard } from "../MovieCard/MovieCard.tsx";
+import type { IMovie } from "../../models/IMovie.ts";
 
-    if(loading) return <div>Loading...</div>
-    if(!movies.length) return <div>No movies found</div>
+interface MoviesListProps {
+    movies: IMovie[];
+}
+
+export const MoviesList = ({ movies }: MoviesListProps) => {
+    if (!movies.length) return <div>No movies found</div>;
 
     return (
         <div className={'grid'}>
-            {movies.map((m) =>
-                <MovieCard key={m.id} movie={m} />)}
+            {movies.map((m) => <MovieCard key={m.id} movie={m} />)}
         </div>
     );
 };
