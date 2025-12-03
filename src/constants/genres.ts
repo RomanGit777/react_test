0ef3fbfda9b33a4d8a2a9158c2744520e0e466cd
RomanGit777 <rsavenko91@gmail.com>
2025-12-03 13:15:44 +0100

    working with badges

---
+++ src/constants/genres.ts
@@ -0,0 +1,14 @@
+export const GENRE_MAP: Record<number, string> = {
+    28: "Action",
+    12: "Adventure",
+    16: "Animation",
+    35: "Comedy",
+    80: "Crime",
+    18: "Drama",
+    14: "Fantasy",
+    27: "Horror",
+    9648: "Mystery",
+    10749: "Romance",
+    878: "Sci-Fi",
+    53: "Thriller",
+};
