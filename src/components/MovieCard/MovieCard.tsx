f960a2ecda218ed20ae48240a06f40b5850f075c
RomanGit777 <rsavenko91@gmail.com>
2025-12-03 13:15:44 +0100

    working with badges

--- src/components/MovieCard/MovieCard.tsx
+++ src/components/MovieCard/MovieCard.tsx
@@ -1,6 +1,8 @@
 import './MovieCard.css'
 import type {IMovie} from "../../models/IMovie.ts";
 import {useNavigate} from "react-router-dom";
+import {Badge} from "reactstrap";
+import {GENRE_MAP} from "../../constants/genres.ts";
 
 
 interface MovieCardProps {
@@ -23,7 +25,11 @@ let navigate = useNavigate();
 
                 <div className="overlay">
                         <div className="overlay-top">
-                            <span className="badge">Genre</span>
+                           <span className={'badge'}> {movie.genre_ids.map(id => (
+                                <Badge key={id} color="info" className="me-1">
+                                    {GENRE_MAP[id]}
+                                </Badge>
+                            ))}</span>
                             <span className="rating">
                                 <svg width="5" height="5" viewBox="0 0 7 6" fill="none" xmlns="http://www.w3.org/2000/svg">
 <path d="M3.05423 0L3.77524 2.21902H6.10846L4.22084 3.59045L4.94185 5.80948L3.05423 4.43804L1.16662 5.80948L1.88762 3.59045L1.14441e-05 2.21902H2.33323L3.05423 0Z" fill="#E5E748"/>
