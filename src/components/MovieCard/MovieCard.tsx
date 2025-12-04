7819f62e1bf26ee23cddbe8f385ba10711325d85
RomanGit777 <rsavenko91@gmail.com>
2025-12-04 12:47:31 +0100

    done with reorganization of StarsRating.tsx in MovieCard.tsx

--- src/components/MovieCard/MovieCard.tsx
+++ src/components/MovieCard/MovieCard.tsx
@@ -4,6 +4,7 @@ import {useNavigate} from "react-router-dom";
 import {Badge} from "reactstrap";
 import {GENRE_MAP} from "../../constants/genres.ts";
 import {PosterPreview} from "../PosterPreview/PosterPreview.tsx";
+import {StarsRating} from "../StarsRating/StarsRating.tsx";
 
 
 interface MovieCardProps {
@@ -28,12 +29,7 @@ let navigate = useNavigate();
                                     {GENRE_MAP[id]}
                                 </Badge>
                             ))}</span>
-                            <span className="rating">
-                                <svg width="5" height="5" viewBox="0 0 7 6" fill="none" xmlns="http://www.w3.org/2000/svg">
-<path d="M3.05423 0L3.77524 2.21902H6.10846L4.22084 3.59045L4.94185 5.80948L3.05423 4.43804L1.16662 5.80948L1.88762 3.59045L1.14441e-05 2.21902H2.33323L3.05423 0Z" fill="#E5E748"/>
-</svg>
-                                <span>{movie.vote_average}</span>
-                            </span>
+                            <StarsRating rating={movie.vote_average} className={'rating'}/>
                         </div>
 
                         <h4 className="overlay-title">{movie.title}</h4>
