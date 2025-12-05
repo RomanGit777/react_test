222508e610c54210da0ab7189fd6b285bf15b75d
RomanGit777 <rsavenko91@gmail.com>
2025-12-05 14:40:17 +0100

    adding new route MovieGenre.tsx

--- src/router/routes.tsx
+++ src/router/routes.tsx
@@ -2,10 +2,12 @@ import {createBrowserRouter} from "react-router-dom";
 import MainLayout from "../layout/MainLayout.tsx";
 import {MoviesPage} from "../pages/MoviesPage.tsx";
 import {MovieInfo} from "../components/MovieInfo/MovieInfo.tsx";
+import {MovieGenre} from "../components/MovieGenre/MovieGenre.tsx";
 
 export const routes = createBrowserRouter([
     {path:'/', element: <MainLayout/>, children:[
             {index: true, element: <MoviesPage/>},
-            {path: 'movie/:id', element: <MovieInfo/>}
+            {path: 'movie/:id', element: <MovieInfo/>},
+            {path: 'genre/:id', element: <MovieGenre/>}
         ]}
 ])
