044490a1ed1f7363bf23cf8e06b1ef095ac7aef0
RomanGit777 <rsavenko91@gmail.com>
2025-12-05 13:23:24 +0100

    added getGenres request

--- src/api/getMovies.ts
+++ src/api/getMovies.ts
@@ -1,6 +1,7 @@
 import axios from "axios"
 import type {IMovie} from "../models/IMovie.ts";
 import type {IMoviesResponse} from "../models/IMoviesResponse.ts";
+import type {IGenres} from "../models/IGenres.ts";
 
 export const getMovies = async (): Promise<IMovie[]> => {
     const {data} = await axios.get<IMoviesResponse>
@@ -10,5 +11,10 @@ export const getMovies = async (): Promise<IMovie[]> => {
 export const getMovieById = async (id: string): Promise<IMovie> => {
     const {data} = await axios.get
     (`https://api.themoviedb.org/3/movie/${id}?api_key=${import.meta.env.VITE_TMDB_API_KEY}`);
+    return data
+}
+export const getGenres = async (): Promise<IGenres[]> => {
+    const {data} = await axios.get
+    (`https://api.themoviedb.org/3/genre/movie/list?api_key=${import.meta.env.VITE_TMDB_API_KEY}`);
     return data
 }
