6e3c064dee2c2cc8de1e4a87154ada1e22bc1142
RomanGit777 <rsavenko91@gmail.com>
2025-12-06 19:27:46 +0100

    working on new route MovieGenre.tsx

--- src/api/getMovies.ts
+++ src/api/getMovies.ts
@@ -18,3 +18,8 @@ export const getGenres = async (): Promise<IGenresResponse> => {
     (`https://api.themoviedb.org/3/genre/movie/list?api_key=${import.meta.env.VITE_TMDB_API_KEY}`);
     return data
 }
+export const getMoviesByGenre = async (genre_ids: string): Promise<IMovie[]> => {
+    const {data} = await axios.get<IMoviesResponse>
+    (`https://api.themoviedb.org/3/discover/movie?with_genres=${genre_ids}&api_key=${import.meta.env.VITE_TMDB_API_KEY}`);
+    return data.results
+}
