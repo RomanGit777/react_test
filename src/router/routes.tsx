9c1fa624f3436d191ebc7f6f55f0d4227ae099d5
RomanGit777 <rsavenko91@gmail.com>
2025-12-08 18:24:00 +0100

    added path to SearchPage.tsx

--- src/router/routes.tsx
+++ src/router/routes.tsx
@@ -3,11 +3,13 @@ import MainLayout from "../layout/MainLayout.tsx";
 import {MoviesPage} from "../pages/MoviesPage.tsx";
 import {MovieInfo} from "../components/MovieInfo/MovieInfo.tsx";
 import {MovieGenre} from "../components/MovieGenre/MovieGenre.tsx";
+import {SearchPage} from "../pages/SearchPage.tsx";
 
 export const routes = createBrowserRouter([
     {path:'/', element: <MainLayout/>, children:[
             {index: true, element: <MoviesPage/>},
             {path: 'movie/:id', element: <MovieInfo/>},
-            {path: 'genres/:id', element: <MovieGenre/>}
+            {path: 'genres/:id', element: <MovieGenre/>},
+            {path: 'search', element: <SearchPage/>}
         ]}
 ])
