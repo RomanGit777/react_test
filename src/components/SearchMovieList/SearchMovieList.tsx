f4676e7b95a43903161af8ac9d2fd213d19716f4
RomanGit777 <rsavenko91@gmail.com>
2025-12-09 18:02:23 +0100

    adding pagination and design

---
+++ src/components/SearchMovieList/SearchMovieList.tsx
@@ -0,0 +1,16 @@
+import {SearchMovieCard} from "../SearchMovieCard/SearchMovieCard.tsx";
+import type {IMovieSearch} from "../../models/IMovieSearch.ts";
+
+interface SearchMoviesListProps {
+    movies: IMovieSearch[];
+}
+export const SearchMovieList= ({ movies }: SearchMoviesListProps) => {
+    if (!movies.length) return <div>No movies found</div>;
+    return (
+        <div className={'grid-search-page'}>
+            {movies.map((m) => <SearchMovieCard key={m.id} movie={m} />)}
+        </div>
+    );
+}
+
+
