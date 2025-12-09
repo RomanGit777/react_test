6b33baeb42a379e112557a1a73db9f9d2aea6597
RomanGit777 <rsavenko91@gmail.com>
2025-12-09 17:29:48 +0100

    added design to cards in SearchPage.tsx

--- src/pages/SearchPage.tsx
+++ src/pages/SearchPage.tsx
@@ -1,13 +1,17 @@
 import './SearchPage.css'
-import {useSearchParams} from "react-router-dom";
+import {useNavigate, useSearchParams} from "react-router-dom";
 import {useEffect, useState} from "react";
 import {getMoviesBySearch} from "../api/getMovies.ts";
 import type {IMovieSearch} from "../models/IMovieSearch.ts";
 import {PosterPreview} from "../components/PosterPreview/PosterPreview.tsx";
+import {GenreBadge} from "../components/GenreBadge/GenreBadge.tsx";
+import {GENRE_MAP} from "../constants/genres.ts";
+import {StarsRating} from "../components/StarsRating/StarsRating.tsx";
 
 export const SearchPage = () => {
    const [params] = useSearchParams();
    const query = params.get("query") ?? "";
+   const navigate = useNavigate();
 
     const [movies, setMovies] = useState<IMovieSearch[]>([]);
     const [loading, setLoading] = useState(false);
@@ -23,13 +27,47 @@ export const SearchPage = () => {
             .finally(() => setLoading(false));
     }, [query])
     return (
-        <div className={'search-page-wrapper'}>
+        <div className="search-page-wrapper">
             {loading && <div>Loading...</div>}
-            {!loading && movies.map((movie) =>
-                <div key={movie.id}>
-                    {movie.title}
-                    <PosterPreview title={movie.title} posterPath={movie.poster_path}/>
-                </div>)}
+
+            {!loading && movies.map((movie) => (
+                <div
+                    key={movie.id}
+                    className="card"
+                    role="button"
+                    tabIndex={0}
+                    aria-label={movie.title}
+                    onClick={() => navigate(`/movie/${movie.id}`)}
+                >
+                    <PosterPreview
+                        title={movie.title}
+                        posterPath={movie.poster_path}
+                        className="card-img"
+                    />
+
+                    <div className="overlay">
+                        <div className="overlay-top">
+                        <span className="badge">
+                            {movie.genre_ids!.map((id) => (
+                                <GenreBadge
+                                    key={id}
+                                    id={id}
+                                    label={GENRE_MAP[id]}
+                                    className="me-1"
+                                />
+                            ))}
+                        </span>
+
+                            <StarsRating
+                                rating={movie.vote_average}
+                                className="rating"
+                            />
+                        </div>
+
+                        <h4 className="overlay-title">{movie.title}</h4>
+                        {/*<p className="overlay-desc">{shortOverview}</p>*/}
+                    </div>
+                </div>
+            ))}
         </div>
-    );
-};
+    )};
