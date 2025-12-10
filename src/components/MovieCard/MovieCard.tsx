97123dd304596ece2c376e9a88de6da805f4431d
RomanGit777 <rsavenko91@gmail.com>
2025-12-10 22:08:47 +0100

    added movieInfo to MovieCard.tsx and SearchMovieCard.tsx

--- src/components/MovieCard/MovieCard.tsx
+++ src/components/MovieCard/MovieCard.tsx
@@ -1,10 +1,6 @@
 import './MovieCard.css'
 import type {IMovie} from "../../models/IMovie.ts";
-import {useNavigate} from "react-router-dom";
-import {GENRE_MAP} from "../../constants/genres.ts";
-import {PosterPreview} from "../PosterPreview/PosterPreview.tsx";
-import {StarsRating} from "../StarsRating/StarsRating.tsx";
-import {GenreBadge} from "../GenreBadge/GenreBadge.tsx";
+import {MovieInfo} from "../MovieInfo/MovieInfo.tsx";
 
 
 interface MovieCardProps {
@@ -12,29 +8,12 @@ interface MovieCardProps {
 }
 
 export const MovieCard = ({movie}: MovieCardProps) => {
-let navigate = useNavigate();
 
     const shortOverview = movie.overview ? movie.overview.slice(0, 80) + "…" : "";
 
-
     return (
-            <div className="card" role="button" tabIndex={0} aria-label={movie.title}
-                 onClick={() => navigate(`/movie/${movie.id}`)} >
-                    <PosterPreview title={movie.title} posterPath={movie.poster_path} className={'card-img'} />
-
-                <div className="overlay">
-                        <div className="overlay-top">
-                            <span className={'badge'}>
-                                {movie.genre_ids!.map(id => (
-                                    <GenreBadge key={id} id={id} label={GENRE_MAP[id]} className="me-1"/>
-                                ))}
-                            </span>
-                            <StarsRating rating={movie.vote_average} className={'rating'}/>
-                        </div>
-
-                        <h4 className="overlay-title">{movie.title}</h4>
-                        <p className="overlay-desc">{shortOverview}</p>
-                    </div>
-        </div>
+            <>
+                <MovieInfo movie={movie} shortOverview={shortOverview}/>
+            </>
     );
 };
