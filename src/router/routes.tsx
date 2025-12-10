2481c880a0a7f3717acbf4a90699892013f2c465
RomanGit777 <rsavenko91@gmail.com>
2025-12-10 21:56:56 +0100

    changed name movieInfo to MovieDetails.tsx

--- src/router/routes.tsx
+++ src/router/routes.tsx
@@ -1,14 +1,14 @@
 import {createBrowserRouter} from "react-router-dom";
 import MainLayout from "../layout/MainLayout.tsx";
 import {MoviesPage} from "../pages/MoviesPage.tsx";
-import {MovieInfo} from "../components/MovieInfo/MovieInfo.tsx";
 import {MovieGenre} from "../components/MovieGenre/MovieGenre.tsx";
 import {SearchPage} from "../pages/SearchPage.tsx";
+import {MovieDetails} from "../components/MovieDetails/MovieDetails.tsx";
 
 export const routes = createBrowserRouter([
     {path:'/', element: <MainLayout/>, children:[
             {index: true, element: <MoviesPage/>},
-            {path: 'movie/:id', element: <MovieInfo/>},
+            {path: 'movie/:id', element: <MovieDetails/>},
             {path: 'genres/:id', element: <MovieGenre/>},
             {path: 'search', element: <SearchPage/>}
         ]}
