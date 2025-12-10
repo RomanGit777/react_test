12f6a82154f7d82f75b63ff49b20850a1bc85f88
RomanGit777 <rsavenko91@gmail.com>
2025-12-10 22:01:17 +0100

    create MovieInfo.tsx component

---
+++ src/components/MovieInfo/MovieInfo.tsx
@@ -0,0 +1,35 @@
+import {PosterPreview} from "../PosterPreview/PosterPreview.tsx";
+import {GenreBadge} from "../GenreBadge/GenreBadge.tsx";
+import {GENRE_MAP} from "../../constants/genres.ts";
+import {StarsRating} from "../StarsRating/StarsRating.tsx";
+import {useNavigate} from "react-router-dom";
+import type {IMovieSearch} from "../../models/IMovieSearch.ts";
+import type { IMovie } from "../../models/IMovie.ts";
+export interface MovieInfoProps {
+    movie: IMovie | IMovieSearch;
+    shortOverview?: string;
+}
+export const MovieInfo = ({movie,shortOverview}: MovieInfoProps) => {
+    let navigate = useNavigate();
+
+    return (
+        <div className="card" role="button" tabIndex={0} aria-label={movie.title}
+             onClick={() => navigate(`/movie/${movie.id}`)} >
+            <PosterPreview title={movie.title} posterPath={movie.poster_path} className={'card-img'} />
+
+            <div className="overlay">
+                <div className="overlay-top">
+                            <span className={'badge'}>
+                                {movie.genre_ids!.map(id => (
+                                    <GenreBadge key={id} id={id} label={GENRE_MAP[id]} className="me-1"/>
+                                ))}
+                            </span>
+                    <StarsRating rating={movie.vote_average} className={'rating'}/>
+                </div>
+
+                <h4 className="overlay-title">{movie.title}</h4>
+                <p className="overlay-desc">{shortOverview}</p>
+            </div>
+        </div>
+    );
+};
