357e750aa58fc5f1824c21aaeddf87c570510dea
RomanGit777 <rsavenko91@gmail.com>
2025-12-17 21:42:11 +0100

    working with responsive design for tablet / header

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,6 @@
 import './styles/global.css'
-import "./styles/style-0-480.css";
+import './styles/style-0-480.css';
+import './styles/style-481-768.css'
 import {createRoot} from 'react-dom/client'
 import {RouterProvider} from "react-router-dom";
 import {routes} from "./router/routes.tsx";
