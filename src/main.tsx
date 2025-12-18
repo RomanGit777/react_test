29b5d9303ec81f28ed1df8bbe007ddbe7f686880
RomanGit777 <rsavenko91@gmail.com>
2025-12-18 11:03:01 +0100

    responsive design for desktops /

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import './styles/global.css'
 import './styles/style-0-480.css';
 import './styles/style-481-768.css'
+import './styles/style-769-1279.css'
 import {createRoot} from 'react-dom/client'
 import {RouterProvider} from "react-router-dom";
 import {routes} from "./router/routes.tsx";
