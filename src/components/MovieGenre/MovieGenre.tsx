7332a0397845363e816bc150f2d33e53e1fed6c1
RomanGit777 <rsavenko91@gmail.com>
2025-12-19 11:55:52 +0100

    tanStack for MovieGenre.tsx done

--- src/components/MovieGenre/MovieGenre.tsx
+++ src/components/MovieGenre/MovieGenre.tsx
@@ -1,25 +1,18 @@
 import {useParams} from "react-router-dom";
-import {useEffect, useState} from "react";
-import {getMoviesByGenre} from "../../api/getMovies.ts";
-import type {IMovie} from "../../models/IMovie.ts";
 import {MoviesList} from "../MoviesList/MoviesList.tsx";
 import {usePagination} from "../../hooks/UsePagination.tsx";
 import {Pagination} from "../Pagination/Pagination.tsx";
+import {useMovieGenre} from "../../queries/useMovieGenre.ts";
 
 
 export const MovieGenre = () => {
     const {id} = useParams();
-    const [movies, setMovies] = useState<IMovie[]>([]);
-    const [loading, setLoading] = useState(true);
-    useEffect(() => {
-        getMoviesByGenre(String(id)).then(mg => setMovies(mg))
-        .catch((err) => console.log("Failed to load movies", err))
-        .finally(() => setLoading(false));
-    }, [id])
+    const {data: movies, isLoading, error } = useMovieGenre(id);
 
-    const {currentPage,totalPages,goToPage,currentItems} = usePagination(movies);
-    if (loading) return <div>Loading...</div>;
-    if (!movies.length) return <div>No movies found</div>;
+    const {currentPage,totalPages,goToPage,currentItems} = usePagination(movies ?? []);
+    if (isLoading) return <div>Loading...</div>;
+    if (error) return <div>Error: {error.message}</div>;
+    if (!movies?.length) return <div>No movies found</div>;
     return (
         <div className={'genres-page-wrapper'}>
             <MoviesList movies={currentItems} />
