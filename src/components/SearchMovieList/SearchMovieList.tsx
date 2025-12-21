5b3946aa41acf67e3f96f30495aa90883821d456
RomanGit777 <rsavenko91@gmail.com>
2025-12-21 14:03:32 +0100

    styl for fail page

--- src/components/SearchMovieList/SearchMovieList.tsx
+++ src/components/SearchMovieList/SearchMovieList.tsx
@@ -6,7 +6,9 @@ interface SearchMoviesListProps {
     movies: IMovieSearch[];
 }
 export const SearchMovieList= ({ movies }: SearchMoviesListProps) => {
-    if (!movies.length) return <div>No movies found</div>;
+    if (!movies.length) return <div className={'failPage'}>
+        <p>We couldn't find anything</p>
+        <p>Are there any grammatical errors in the request?</p></div> ;
     return (
         <div className={'grid-search-page'} id={'grid-search-page'}>
             {movies.map((m) => <SearchMovieCard key={m.id} movie={m} />)}
