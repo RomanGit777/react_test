4ffbdb3faab5770f3c6cba8600df0d33a5de7e2a
RomanGit777 <rsavenko91@gmail.com>
2025-12-22 20:58:05 +0100

    organizing project / renamed movie genre to page

--- src/router/routes.tsx
+++ src/router/routes.tsx
@@ -1,15 +1,15 @@
 import {createBrowserRouter} from "react-router-dom";
 import MainLayout from "../layout/MainLayout.tsx";
 import {MoviesPage} from "../pages/MoviesPage/MoviesPage.tsx";
-import {MovieGenre} from "../components/MovieGenre/MovieGenre.tsx";
 import {SearchPage} from "../pages/SearchPage/SearchPage.tsx";
 import {MovieDetails} from "../components/MovieDetails/MovieDetails.tsx";
+import {MovieGenresPage} from "../pages/MovieGenresPage/MovieGenresPage.tsx";
 
 export const routes = createBrowserRouter([
     {path:'/', element: <MainLayout/>, children:[
             {index: true, element: <MoviesPage/>},
             {path: 'movie/:id', element: <MovieDetails/>},
-            {path: 'genres/:id', element: <MovieGenre/>},
+            {path: 'genres/:id', element: <MovieGenresPage/>},
             {path: 'search', element: <SearchPage/>}
         ]}
 ])
