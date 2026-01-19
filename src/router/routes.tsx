46f3aed71bee99effb52d0a84a3641434db93334
RomanGit777 <rsavenko91@gmail.com>
2026-01-19 14:13:18 +0100

    changed browser router to hash router

--- src/router/routes.tsx
+++ src/router/routes.tsx
@@ -1,11 +1,11 @@
-import {createBrowserRouter} from "react-router-dom";
+import {createHashRouter} from "react-router-dom";
 import MainLayout from "../layout/MainLayout.tsx";
 import {MoviesPage} from "../pages/MoviesPage/MoviesPage.tsx";
 import {SearchPage} from "../pages/SearchPage/SearchPage.tsx";
 import {MovieDetails} from "../components/MovieDetails/MovieDetails.tsx";
 import {MovieGenresPage} from "../pages/MovieGenresPage/MovieGenresPage.tsx";
 
-export const routes = createBrowserRouter([
+export const routes = createHashRouter([
     {path:'/', element: <MainLayout/>, children:[
             {index: true, element: <MoviesPage/>},
             {path: 'movie/:id', element: <MovieDetails/>},
